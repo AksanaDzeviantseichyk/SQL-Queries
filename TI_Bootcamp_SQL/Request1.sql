e3278e1e5212e1a9b76b63c426c81c85cf5c3d5a
Aksana Dzeviantseichyk <a.dzeviantseichyk@gmail.com>
2023-04-14 11:42:46 +0200

    update

--- TI_Bootcamp_SQL/Request1.sql
+++ TI_Bootcamp_SQL/Request1.sql
@@ -1,10 +1,7 @@
 USE companydb;
 SELECT 
-				employee.id, first_name + ' ' + last_name AS 'full name',
-				name AS 'position name', rate
-FROM 
-				employee
-INNER JOIN
-				position
-ON 
-				project_id = position.id;
+    employee.id, first_name + ' ' + last_name AS 'full name',
+    name AS 'position name', rate
+FROM employee
+INNER JOIN position
+ON project_id = position.id;
--- TI_Bootcamp_SQL/Request4.sql
+++ TI_Bootcamp_SQL/Request4.sql
@@ -1,14 +1,11 @@
 USE companydb;
 SELECT 
-				CONCAT(emp.first_name, ' ', emp.last_name) AS 'employee full name', 
-				pr.name AS 'project name',
-				pr.max_sum_rate -(pos.rate + ISNULL(SUM(eq.price),0)) AS difference
-FROM 
-				employee emp
+    CONCAT(emp.first_name, ' ', emp.last_name) AS 'employee full name', 
+    pr.name AS 'project name',
+    pr.max_sum_rate -(pos.rate + ISNULL(SUM(eq.price),0)) AS difference
+FROM employee emp
 JOIN project pr ON emp.project_id = pr.id
 JOIN position pos ON pos.id = emp.position_id
 LEFT JOIN equipment eq ON eq.user_id = emp.id
-GROUP BY 
-    emp.first_name, emp.last_name, pr.name, pr.max_sum_rate, pos.rate
-HAVING 
-    (pos.rate + ISNULL(SUM(eq.price),0)/12) > pr.max_sum_rate
+GROUP BY emp.first_name, emp.last_name, pr.name, pr.max_sum_rate, pos.rate
+HAVING (pos.rate + ISNULL(SUM(eq.price),0)/12) > pr.max_sum_rate
