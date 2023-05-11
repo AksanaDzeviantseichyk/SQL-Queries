a9fa57a8a38e8de13f81f29a270ede92e7fb366e
Aksana Dzeviantseichyk <a.dzeviantseichyk@gmail.com>
2023-05-11 14:13:57 +0200

    request 4 is updated

--- TI_Bootcamp_SQL/Request4.sql
+++ TI_Bootcamp_SQL/Request4.sql
@@ -2,7 +2,7 @@ USE companydb;
 SELECT 
     CONCAT(emp.first_name, ' ', emp.last_name) AS 'employee full name', 
     pr.name AS 'project name',
-    pr.max_sum_rate -(pos.rate + ISNULL(SUM(eq.price),0)) AS difference
+    pr.max_sum_rate -(pos.rate + ISNULL(SUM(eq.price),0)/12) AS difference
 FROM employee emp
 JOIN project pr ON emp.project_id = pr.id
 JOIN position pos ON pos.id = emp.position_id
